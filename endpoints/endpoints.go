67ea361ca23f7c091c0fb91689a7eede5e318a4a
lucas <lucas.sales@sead.local>
2024-07-29 11:09:48 -0300

    .

--- endpoints/endpoints.go
+++ endpoints/endpoints.go
@@ -1,1 +1,44 @@
 package endpoints
+
+import (
+	"net/http"
+	"p2p/actions"
+)
+
+var ACTIONS *actions.Actions
+
+func HandleBegin(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		ACTIONS.Connect()
+	} else {
+		http.Error(w, "", http.StatusMethodNotAllowed)
+	}
+}
+
+func HandleFile(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Invalid", http.StatusBadRequest)
+			return
+		}
+
+		filename := r.FormValue("filename")
+		ACTIONS.InsertFile(filename)
+
+	} else if r.Method == http.MethodGet {
+		filename := r.URL.Query().Get("filename")
+		ACTIONS.GetFile(filename)
+
+	} else {
+		http.Error(w, "", http.StatusMethodNotAllowed)
+	}
+}
+
+func HandleLeave(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+
+	} else {
+		http.Error(w, "", http.StatusMethodNotAllowed)
+	}
+}
