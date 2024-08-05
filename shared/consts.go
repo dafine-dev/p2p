875b264dcd646420fafcd64d9e346d9907941ffd
dafine-dev <lucas.sales114@hotmail.com>
2024-08-04 22:08:29 -0300

    .

--- shared/consts.go
+++ shared/consts.go
@@ -5,6 +5,6 @@ import "syscall"
 const PORT = 9000
 
 var BROADCAST_ADDR syscall.SockaddrInet4 = syscall.SockaddrInet4{
-	Addr: [4]byte{255, 255, 255, 255},
+	Addr: [4]byte{192, 168, 1, 255},
 	Port: PORT,
 }
